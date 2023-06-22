69c792024efd75d7a0a90c4ae228677c4ab4ff95
Jonel Capa <jonelcapa@yahoo.com>
2023-06-22 14:10:07 +0800

    Update todosSlice with API calls

--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -7,12 +7,21 @@ export const fetchTodos = createAsyncThunk("todos/fetchAll", () =>
 );
 export const deleteTodo = createAsyncThunk("todo/delete", (id) => {
   // TODO: return a call  to corresponding API method i.e. todoAPI.fetchAll()
+  const response = todoAPI.deleteOne(id);
+  const todo = response.data;
+  return todo;
 });
 export const updateTodo = createAsyncThunk("todo/update", (payload) => {
   // TODO: return a call  to corresponding API method i.e. todoAPI.fetchAll()
+  const response = todoAPI.updateOne(payload.id, payload.todo);
+  const todo = response.data;
+  return todo;
 });
 export const addTodo = createAsyncThunk("todo/add", (todo) => {
   // TODO: return a call  to corresponding API method i.e. todoAPI.fetchAll()
+  const response = todoAPI.createOne(todo);
+  const todo = response.data;
+  return todo;
 });
 
 const initialState = [];
@@ -33,10 +42,14 @@ const todosSlice = createSlice({
 
     builder.addCase(addTodo.fulfilled, (state, action) => {
       // TODO: Finish the code for adding todo
+      const index = state.findIndex((todo) => todo.id === action.meta.arg);
+      if (index !== -1) state.splice(index, 1, action.payload);
     });
 
     builder.addCase(updateTodo.fulfilled, (state, action) => {
       // TODO: Finish the code for updating todo
+      const index = state.findIndex((todo) => todo.id === action.meta.arg);
+      if (index !== -1) state.splice(index, 1, action.payload);
     });
   },
 });
